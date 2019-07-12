afd6ec285b292f1d0b2b7ace02ecb1b807ccfa6c
赫敏璋 <loongsking@hotmail.com>
2019-07-13 05:35:16 +0800

    add c# client test examply

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task running()
+        {
+            var connection = new HubConnectionBuilder().WithUrl("https://localhost:44357/chathub").Build();
+            await connection.StartAsync();
+            Console.WriteLine("client connection state:" + connection.State);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.WriteLine("input yes or no:");
+                string input = Console.ReadLine();
+                if (input.ToUpper().IndexOf("Y") != -1)
+                {
+                    Console.WriteLine("Ok! running... ...");
+                    running().Wait();
+                    continue;
+                }
+                else if (input.ToUpper().IndexOf("N") != -1)
+                {
+                    Console.WriteLine("So, break!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("So, next!");
+                }
+            }
+        }
+    }
+}
