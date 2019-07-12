ca7481af7c87ea8e6abfb2f6c1530bda48dffdf5
赫敏璋 <loongsking@hotmail.com>
2019-07-13 04:46:13 +0800

    Update

---
+++ ConsoleApplication1/WebApplication1/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Hubs
+{
+    public class ChatHub : Hub<IChatClient>
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.Caller.ReceiveMessage(user + message);
+        }
+
+        public async Task SendMessageToCaller(string message)
+        {
+            await Clients.Caller.ReceiveMessage("Ok! I already get" + message);
+        }
+    }
+}
